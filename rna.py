21146cbbf99c954085f4bc2ff6214f90bb96a69f
maindev <systemdevcloud@live.co.uk>
2017-02-23 22:47:17 +0000

    update rna context

--- rna.py
+++ rna.py
@@ -1,18 +1,19 @@
-# Counting DNA Nucleotides
+# Transcribing DNA into RNA
 
 '''
 
-http://rosalind.info/problems/dna/
+An RNA string is a string formed from the alphabet containing 'A', 'C', 'G', and 'U'.
 
-A string is simply an ordered collection of symbols selected from some alphabet and formed into a word; the length of a string is the number of symbols that it contains.
+Given a DNA string t
+corresponding to a coding strand, its transcribed RNA string u is formed by replacing all occurrences of 'T' in t with 'U' in u
 
-An example of a length 21 DNA string (whose alphabet contains the symbols 'A', 'C', 'G', and 'T') is "ATGCTTCAGAAAGGTCTTACG."
+.
 
-Given: A DNA string s
+Given: A DNA string t
 
-of length at most 1000 nt.
+having length at most 1000 nt.
 
-Return: Four integers (separated by spaces) counting the respective number of times that the symbols 'A', 'C', 'G', and 'T' occur in s
+Return: The transcribed RNA string of t
 .
 
 '''
